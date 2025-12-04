e824154699ec79958badd1eb63b23acfc527e706
cbarlin <41846367+cbarlin@users.noreply.github.com>
2025-12-04 18:16:27 +1000

    Optimise day 2

--- src/main/java/io/github/cbarlin/aoc2025/Application.java
+++ src/main/java/io/github/cbarlin/aoc2025/Application.java
@@ -3,7 +3,7 @@ package io.github.cbarlin.aoc2025;
 public final class Application {
     static void main() {
         Day01.runDay();
-        // Day02.runDay();
+        Day02.runDay();
         Day03.runDay();
         Day04.runDay();
     }
--- src/main/java/io/github/cbarlin/aoc2025/Day02.java
+++ src/main/java/io/github/cbarlin/aoc2025/Day02.java
@@ -5,45 +5,78 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.stream.IntStream;
 
 public final class Day02 {
     private static final String testInput = "day02/test.txt";
     private static final String realInput = "day02/real.txt";
+    // Let's just do this once...
+    private static final long[] POWERS_OF_TEN = IntStream.range(0, 19)
+            .mapToLong(i -> (long) Math.pow(10, i))
+            .filter(l -> l >= 0)
+            .toArray();
 
     private record Result (
             long byHalvesSum,
             long anyRepeatsSum
     ) {}
 
-    private static int countUniqueSubstrings(final String input, final int fixedWindowSize) {
-        final Set<String> uniq = HashSet.newHashSet(input.length() / fixedWindowSize);
-        for (int i = 0; i <= input.length() - fixedWindowSize; i+=fixedWindowSize) {
-            uniq.add(input.substring(i, i + fixedWindowSize));
+    private static int numberOfDigits(final long x) {
+        int length = 0;
+        long temp = 1;
+        while (temp <= x) {
+            length++;
+            temp *= 10;
         }
-        return uniq.size();
+        return length;
     }
 
+    // Generate possible numbers rather than looping to work it out
+    // Basically, for e.g. 123123 it's 123 * (10 ^ 3 + 1)
+    // Or 121212 it's 12 * (10^4 + 10^2 + 1)
+    // Work out, from possible multipliers, valid "base" numbers to multiply in!
     private static Result sumNumbersWithRepeating(final long start, final long end) {
         long byHalvesSum = 0;
         long anyRepeatsSum = 0;
-        for (long i = start; i <= end; i++) {
-            final String str = String.valueOf(i);
-            final int mid = str.length() / 2;
-            if (str.substring(0, mid).equals(str.substring(mid))) {
-                byHalvesSum += i;
-                anyRepeatsSum += i;
-                continue;
-            }
-            for (int l = 1; l <= mid; l ++) {
-                if (str.length() % l == 0) {
-                    final int uniq = countUniqueSubstrings(str, l);
-                    if (uniq == 1) {
-                        anyRepeatsSum += i;
-                        break;
+
+        final Set<Long> visited = new HashSet<>();
+        final int minDigits = numberOfDigits(start);
+        final int maxDigits = numberOfDigits(end);
+
+        // Loop between the total digit lengths (e.g the start is 99 and end is 12345 - do 2, 3, 4, 5)
+        for (int totalLength = minDigits; totalLength <= maxDigits; totalLength++) {
+            // Is the current totalLength halvable?
+            final boolean halvable = totalLength % 2 == 0;
+            // How much should we try and increment the power of 10 search?
+            // e.g. 1111111, 1010101
+            for (int powerOfTenIncrement = 1; powerOfTenIncrement <= totalLength / 2; powerOfTenIncrement++) {
+                if (totalLength % powerOfTenIncrement != 0) {
+                    continue;
+                }
+                final boolean isHalfPattern = halvable && powerOfTenIncrement == (totalLength / 2);
+                // Calculate Multiplier.
+                // E.g. totalLength 6, powerOfTenIncrement 2 (121212) -> Multiplier = 10101
+                long multiplier = 0;
+                for (int k = 0; k < totalLength; k += powerOfTenIncrement) {
+                    multiplier += POWERS_OF_TEN[k];
+                }
+                // Work out the "range" of base numbers we can use (e.g. 1038-9863 should have 11 -> 63 as possible bases)
+                final long currentStartBase = Math.max(POWERS_OF_TEN[powerOfTenIncrement - 1], (start + multiplier - 1) / multiplier);
+                final long currentEndBase = Math.min(POWERS_OF_TEN[powerOfTenIncrement] - 1, end / multiplier);
+
+                for (long base = currentStartBase; base <= currentEndBase; base++) {
+                    long val = base * multiplier;
+                    // Only add if not already seen
+                    if (visited.add(val)) {
+                        anyRepeatsSum += val;
+                    }
+                    if (isHalfPattern) {
+                        byHalvesSum += val;
                     }
                 }
             }
         }
+
         return new Result(byHalvesSum, anyRepeatsSum);
     }
 
